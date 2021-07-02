9579a4bd421b1206fadd969127eaa7772f5cf23c
ackrad <egcorak@gmail.com>
2021-07-02 05:22:05 +0300

    Working on UıController not satisfied

---
+++ Assets/Addition/Scripts/Countdown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] int countdowntimer = 3;
+    [SerializeField] Text countdownDisplay;
+
+    // Start is called before the first frame update
+   
+
+
+    public void StartCountdown()
+    {
+
+        StartCoroutine(CountdownToStart());
+
+
+
+    }
+
+
+    IEnumerator CountdownToStart()
+    {
+        while(countdowntimer > 0)
+        {
+            countdownDisplay.text = countdowntimer.ToString();
+            yield return new WaitForSeconds(1f);
+
+            countdowntimer--;
+
+
+
+        }
+
+        countdownDisplay.text = "GO!";
+
+
+    }
+}
---
+++ Assets/Addition/Scripts/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class UIController : Singleton<UIController>
+{
+    [SerializeField] GameObject countdownCanvas;
+
+
+    public UnityEvent StartSliding = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.request().OnGameStarted.AddListener(InvokeStartSlideing);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InvokeStartSlideing()
+    {
+
+        StartCoroutine(CountDown());
+    }
+
+
+
+    private IEnumerator CountDown()
+    {
+        countdownCanvas.SetActive(true);
+        FindObjectOfType<Countdown>().StartCountdown();
+
+        yield return new WaitForSeconds(3f); // TODO bunu serialized yapmak isteyebilirsin
+
+
+        countdownCanvas.SetActive(false);
+
+        StartSliding.Invoke();
+
+
+    }
+}
