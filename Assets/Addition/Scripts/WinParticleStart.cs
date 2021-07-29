cbe96edc44a1252e14c3ebab41486422277caf8c
ackrad <egcorak@gmail.com>
2021-07-29 17:19:28 +0300

    Added particle Effects

---
+++ Assets/Addition/Scripts/WinParticleStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinParticleStart : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GameController.request().OnGameWin.AddListener(StartParticles);
+    }
+
+    private void StartParticles()
+    {
+
+        GetComponent<ParticleSystem>().Play();
+
+
+    }
+
+
+
+
+}
